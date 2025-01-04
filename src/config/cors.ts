4432d6ba42f90a03d9e40e38eb2f69463f8ceec6
Jonhks <jonhgx@gmail.com>
2025-01-03 19:49:51 -0600

    cors error 2

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -16,16 +16,33 @@
 
 import { CorsOptions } from "cors";
 
+// export const corsConfig: CorsOptions = {
+//   origin: (origin, callback) => {
+//     const whiteList = [process.env.FRONTEND_URL];
+//     if (process.argv[2] === "--api") {
+//       whiteList.push(undefined);
+//     }
+//     console.log("Origin:", origin);
+//     console.log("WhiteList:", whiteList);
+
+//     if (whiteList.includes(origin)) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error("Error de CORS"));
+//     }
+//   },
+// };
+
 export const corsConfig: CorsOptions = {
   origin: (origin, callback) => {
     const whiteList = [process.env.FRONTEND_URL];
     if (process.argv[2] === "--api") {
-      whiteList.push(undefined);
+      whiteList.push(undefined); // Permitir solicitudes sin origen (por ejemplo, desde Postman)
     }
     console.log("Origin:", origin);
     console.log("WhiteList:", whiteList);
-
-    if (whiteList.includes(origin)) {
+    if (whiteList.includes(origin) || !origin) {
+      // Permitir solicitudes sin origen
       callback(null, true);
     } else {
       callback(new Error("Error de CORS"));
